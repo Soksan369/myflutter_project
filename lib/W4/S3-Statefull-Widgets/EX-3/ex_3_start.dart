9ef474df4b3c621394bf809c8513e41b8e30b9e8
Soksan369 <147368894+Soksan369@users.noreply.github.com>
2024-11-09 15:52:09 +0700

    ex3

--- lib/W4/S3-Statefull-Widgets/EX-3/ex_3_start.dart
+++ lib/W4/S3-Statefull-Widgets/EX-3/ex_3_start.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
 
-List<String> images = [
+class ImageGallery extends StatefulWidget {
+  const ImageGallery({super.key});
+
+  @override
+  State<ImageGallery> createState() => _ImageGalleryState();
+}
+
+class _ImageGalleryState extends State<ImageGallery> {
+  int _index = 0;
+  final List<String> images = [
   "assets/w4-s2/bird.jpg",
   "assets/w4-s2/bird2.jpg",
-  "assets/w4-s2/insect.jpg",
   "assets/w4-s2/girl.jpg",
+  "assets/w4-s2/insect.jpg",
   "assets/w4-s2/man.jpg",
 ];
 
-void main() => runApp(MaterialApp(
-      debugShowCheckedModeBanner: false, // Why this line ? Can you explain it ?
-      home: Scaffold(
+void _previousImage() {
+  setState(() {
+    _index = (_index - 1 ) % images.length;
+  });
+}
+void _nextImage() {
+  setState(() {
+    _index = (_index + 1) % images.length;
+  });
+}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         backgroundColor: Colors.green[50],
         appBar: AppBar(
           backgroundColor: Colors.green[400],
@@ -19,18 +38,29 @@ void main() => runApp(MaterialApp(
             IconButton(
               icon: const Icon(Icons.navigate_before),
               tooltip: 'Go to the previous image',
-              onPressed: () => {},
+              onPressed: _previousImage,
             ),
             Padding(
               padding: const EdgeInsets.fromLTRB(0, 0, 50, 0),
               child: IconButton(
                 icon: const Icon(Icons.navigate_next),
                 tooltip: 'Go to the next image',
-                onPressed: () => {},
+                onPressed: _nextImage,
               ),
             ),
           ],
         ),
-        body: Image.asset(images[0]),
+    body: Image.asset(
+      images[_index],
+      fit: BoxFit.cover,
       ),
+      );
+  }
+}
+
+
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false, // Why this line ? Can you explain it ?
+  home: ImageGallery(),
     ));
